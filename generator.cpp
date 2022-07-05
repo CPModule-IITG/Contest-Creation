64e366b0205c70397c4ab33634c2ce1afbc17330
Akshaj <67319076+Akshaj-Padmakar@users.noreply.github.com>
2022-07-05 17:54:02 +0530

    .

--- generator.cpp
+++ generator.cpp
@@ -1,33 +1,34 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int rand(int  a, int b) 
-{
-	return a + rand() % (b - a + 1);  // returns a random number belonging to interval [a, b]
+int rand(int  a, int b) {
+	return a + rand() % (b - a + 1);
+	// returns a random number belonging to interval [a, b]
 }
 
 ifstream fin;
 ofstream fout;
 
-void generate(int file) // modify the function according to the testcase you want
-{
-	int testcases=1000;
-	fout<<testcases<<"\n";
-	for(int i=0; i<testcases; i++)
-	{
-		int n=i/10+1;
-		fout<<n<<"\n";
-		for(int j=0; j<n; j++)
-			fout<<j+1<<" ";
-		fout<<"\n";
+void generateTC(int testcases) {
+//write the logic for each testcase here
+
+}
+void generate(int file) {
+
+	int testcases = 1; //For multiple testcases
+	// fout << testcases << "\n";
+
+	for (int i = 0; i < testcases; i++) {
+		generateTC(testcases);
 	}
 }
 
-void create(int file) //don't change this function
-{  
+void create(int file) {
+//don't change this function
 	string a = to_string(file);
-	if (a.size() < 2)
+	if (a.size() < 2) {
 		a = '0' + a;
+	}
 	fout.open("input" + a + ".txt");
 	generate(file);
 	fout.close();
@@ -37,10 +38,10 @@ int main() {
 	srand (time (0));  // to generate different random numbers at each execution step
 	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 
-	int number_of_files; cin>>number_of_files;
-	for (int file = 0; file <number_of_files; file++) {
+	int number_of_files; cin >> number_of_files;
+
+	for (int file = 0; file < number_of_files; file++) {
 		create(file);
 	}
-	return 0;
 
 }
--- solver.cpp
+++ solver.cpp
@@ -1,15 +1,15 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int rand(int  a, int b) 
-{
+int rand(int  a, int b) {
 	return a + rand() % (b - a + 1);  // returns a random number belonging to interval [a, b]
 }
 
 ifstream fin;
 ofstream fout;
 
-void solve() {  //The logic of the problem , just paste your solve function here and replace cin, cout with fin, fout
+void solve() {
+//The logic of the problem , just paste your solve function here and replace cin->fin, cout->fout.
 
 
 }
@@ -17,8 +17,9 @@ void solve() {  //The logic of the problem , just paste your solve function here
 
 void create(int file) {  //don't change this function
 	string a = to_string(file);
-	if (a.size() < 2)
+	if (a.size() < 2) {
 		a = '0' + a;
+	}
 
 	fin.open("input" + a + ".txt");
 	fout.open("output" + a + ".txt");
@@ -36,10 +37,9 @@ int main() {
 	srand (time (0));  // to generate different random numbers at each execution step
 	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 
-	int number_of_files; cin>>number_of_files;
-	for (int file = 0; file <number_of_files; file++) {
+	int number_of_files; cin >> number_of_files;
+	for (int file = 0; file < number_of_files; file++) {
 		create(file);
 	}
-	return 0;
 
 }
